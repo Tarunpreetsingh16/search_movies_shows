68ac813716b57ca5c70e62e399e83aa88c080c98
Tarun <tarunpreetsingh@gmail.com>
2021-08-28 16:55:23 -0400

    adds theme utility

--- src/utilities/theme.js
+++ src/utilities/theme.js
@@ -1,5 +1,5 @@
 export const setupTheme = () => {
     const root = document.documentElement;
-    root.style.setProperty('--main-theme-color', '#C4C4C4');
+    root.style.setProperty('--main-theme-color', '#C4C4C480');
     root.style.setProperty('--main-text-color', '#444242');
 };

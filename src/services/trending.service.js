47ea67a1de7706866689de580382174c7178fab7
Tarun <tarunpreetsingh@gmail.com>
2021-08-28 19:02:52 -0400

    adds service file to fetch trending data

---
+++ src/services/trending.service.js
@@ -0,0 +1,23 @@
+import axiosInstance from "../axiosSetup";
+
+export const getTrendingMovies = async () => {
+    try {
+        const response = await axiosInstance.get(
+            '/trending/movie/day',
+        );
+        return response.data;
+    } catch(err) {
+        console.log(err);
+    }
+};
+
+export const getTrendingShows = async () => {
+    try {
+        const response = await axiosInstance.get(
+            '/trending/tv/day',
+        );
+        return response.data;
+    } catch(err) {
+        console.log(err);
+    }
+};

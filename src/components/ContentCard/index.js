b6ebb2199ce374215d47c9951500de072205d9f5
Tarun <tarunpreetsingh@gmail.com>
2021-08-28 22:09:50 -0400

    inits card component for displaying shows and movies

---
+++ src/components/ContentCard/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const ContentCard = ({details}) => {
+    return (
+        <div className='content-container'></div>
+    );
+};
+
+export default ContentCard;
